cf38f8ceb36c63b87cfe77a9dd60a98a3b29ab4e
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-27 15:55:16 +0200

    Updates

---
+++ precache-manifest.c83b32db07b32e7875216e546642da80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3df11dd0903c53324bb37700496ebe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a1d8fb22a642edea3026",
+    "url": "./static/css/main.aa59063b.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d8fb22a642edea3026",
+    "url": "./static/js/main.26a42f1a.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  },
+  {
+    "revision": "9b7da1704f90d7715f07d2bcf82862d2",
+    "url": "./static/media/The_Persistence_of_Memory.9b7da170.jpg"
+  }
+]);
